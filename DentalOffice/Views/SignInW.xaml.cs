5bb1b8b95dac313dce1b8eb484a61ce3ba0d66f1
CRISTINA-CALIN\Cristina <cristinaioanacalin@gmail.com>
2021-11-03 18:04:27 +0200

    Log in

---
+++ DentalOffice/Views/SignInW.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace DentalOffice.Views
+{
+    /// <summary>
+    /// Interaction logic for SignInW.xaml
+    /// </summary>
+    public partial class SignInW : Window
+    {
+        public SignInW()
+        {
+            
+        }
+    }
+}
