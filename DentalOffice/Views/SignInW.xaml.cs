46ec9fcbc94c6e27534e3259c95e0d35491968af
CRISTINA-CALIN\Cristina <cristinaioanacalin@gmail.com>
2021-11-23 20:35:49 +0200

    Update

--- DentalOffice/Views/SignInW.xaml.cs
+++ DentalOffice/Views/SignInW.xaml.cs
@@ -1,15 +1,66 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using DentalOffice.Views;
+using DentalOffice.ViewModels;
+using DentalOffice.Models;
+using DentalOffice.Models.DataAccesslayer;
+using DentalOffice.Views.DoctorViews;
 
-namespace DentalOffice.Views
+namespace DentalOffice
 {
     /// <summary>
-    /// Interaction logic for SignInW.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class SignInW : Window
+    public partial class MainWindow : Window
     {
-        public SignInW()
+        public MainWindow()
         {
+            InitializeComponent();
+        }
+        
+
+        private void signInBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string Name = this.nameBox.Text.ToString();
+            string Password = this.passwordBox.Password.ToString();
+
+            User user = UserDAL.GetUserPr(Name, Password);
+
+            if(user is null)
+            {
+                MessageBox.Show("User account not found!", "Error",MessageBoxButton.OK,MessageBoxImage.Error);
+            }
+            else if(user.Role == Role.administrator)
+            {
+                AdministratorW adminWindow = new AdministratorW(user);
+                adminWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                DoctorW doctorWindow = new DoctorW(user);
+                doctorWindow.Show();
+                this.Close();
+            }
+
             
+        }
+
+        private void exitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
         }
     }
 }
