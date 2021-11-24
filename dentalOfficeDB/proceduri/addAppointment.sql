470536c1de503ff7d0b2486ce19787997f5ab374
Andrea Puia <49001320+andreapuia@users.noreply.github.com>
2021-11-24 12:19:56 +0200

    adaugare programare in db

---
+++ dentalOfficeDB/proceduri/addAppointment.sql
@@ -0,0 +1,14 @@
+﻿/* procedură ce adaugă o nouă programare, se știe că noua programare nu se va suprapune peste altă programare, returnează indexul noii programări */
+
+CREATE PROCEDURE addAppointmentPr
+	@output INT OUTPUT,
+	@IDpatient INT,
+	@Date DATE,
+	@Hour INT
+AS
+BEGIN
+	
+	INSERT INTO Appointment(IDpatient,Date,Hour,Deleted) VALUES (@IDpatient,@Date,@Hour,'no')
+	SET @output = SCOPE_IDENTITY()
+END
+GO
