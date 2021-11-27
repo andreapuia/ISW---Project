67ab0cb88d201311db5598bfa6cda99f91a7534b
CRISTINA-CALIN\Cristina <cristinaioanacalin@gmail.com>
2021-11-27 10:51:59 +0200

    View Models

---
+++ DentalOffice/ViewModels/AdministratorVM.cs
@@ -0,0 +1,67 @@
+﻿using DentalOffice.Models;
+using DentalOffice.Models.DataAccesslayer;
+using System.Collections.ObjectModel;
+
+namespace DentalOffice.ViewModels
+{
+    public class AdministratorVM : BaseVM
+    {
+        #region private members
+
+        private User selectedDoctor;
+        private User currentAdmin;
+        private string currentAdminText;
+        private Intervention currentIntervention;
+        private ObservableCollection<User> doctorsList;
+        private ObservableCollection<Intervention> interventionsList;
+
+        #endregion
+        
+        #region public properties
+
+        public User CurrentAdmin
+        {
+            get { return this.currentAdmin; }
+            set { this.currentAdmin = value; CurrentAdminText = "Welcome back, " + this.currentAdmin.FullName + "!"; }
+        }
+
+        public string CurrentAdminText
+        {
+            get { return this.currentAdminText; }
+            set { this.currentAdminText = value; OnPropertyChanged("CurrentAdminText"); }
+        }
+
+        public ObservableCollection<User> DoctorsList
+        {
+            get { return this.doctorsList; }
+            set { this.doctorsList = value; OnPropertyChanged("DoctorsList"); }
+        }
+
+        public User SelectedDoctor
+        {
+            get { return this.selectedDoctor; }
+            set { this.selectedDoctor = value; OnPropertyChanged("SelectedDoctor"); }
+
+        }
+
+        public ObservableCollection<Intervention> InterventionsList
+        {
+            get { return this.interventionsList; }
+            set { this.interventionsList = value; OnPropertyChanged("InterventionsList"); }
+        }
+
+        public Intervention CurrentIntervention
+        {
+            get { return this.currentIntervention; }
+            set { this.currentIntervention = value; OnPropertyChanged("CurrentIntervention"); }
+        }
+
+        #endregion
+        
+        public AdministratorVM()
+        {
+            this.DoctorsList = UserDAL.GetDoctorsPr();
+            this.InterventionsList = InterventionDAL.GetInterventionsPr();
+        }
+    }
+}
